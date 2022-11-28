58216325700ccf13bb91d56a618e374f4795bd09
Koddi <r.koddi.moe@gmail.com>
2022-11-28 11:45:20 -0800

    Added break down of game

--- GG.py
+++ GG.py
@@ -1,1 +1,20 @@
 # Guessing Game
+
+#computer picks number at random based on difficulty (easy, medium, hard)
+
+
+#User inputs guess, computer outputs response based on how close answer is to number, if guess is not a number within range computer lets the user know
+
+
+#User gets score reduction with each guess
+
+
+#after guess is correct final score is listed
+
+
+
+
+#later versions could have high score list
+
+
+
