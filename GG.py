f0d5c71517ec980f86ec764ed27191954a5e8a60
Koddi <r.koddi.moe@gmail.com>
2022-11-30 10:33:49 -0800

    Added code for computer to pick a number

--- GG.py
+++ GG.py
@@ -1,9 +1,36 @@
 # Guessing Game
+import random
 
 #computer picks number at random based on difficulty (easy, medium, hard)
 
 
+def difficulty(user_choice):
+    easy = random.randint(1, 5)
+    medium = random.randint(1, 10)
+    hard = random.randint(1, 50)
+
+    if user_choice == "easy":
+        difficulty_setting = easy
+    elif user_choice == "medium":
+        difficulty_setting = medium
+    elif user_choice == "hard":
+        difficulty_setting = hard
+    else:
+        difficulty_setting = easy
+
+    return difficulty_setting
+
+user_difficulty_choice = input("Pick a difficulty, easy, medium, hard: ")
+
+
+computer_num = difficulty(user_difficulty_choice.lower())
+
+
+
 #User inputs guess, computer outputs response based on how close answer is to number, if guess is not a number within range computer lets the user know
+
+user_guess = input("Guess a number! easy (1 - 5), medium (1 - 10), hard (1 - 50): ")
+
 
 
 #User gets score reduction with each guess
