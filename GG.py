1c2fc94787193ee87500f1fb5146df51107bfa10
Koddi-Moe <r.koddi.moe@gmail.com>
2022-12-01 14:33:44 -0800

    Added while loop to set amount of tries

--- GG.py
+++ GG.py
@@ -2,52 +2,56 @@
 import random
 
 #computer picks number at random based on difficulty (easy, medium, hard)
+score = 0
+trys = 5
+while score < trys:
 
 
-def difficulty(user_choice):
-    easy = random.randint(1, 5)
-    medium = random.randint(1, 10)
-    hard = random.randint(1, 50)
+    def difficulty(user_choice):
+        easy = random.randint(1, 5)
+        medium = random.randint(1, 10)
+        hard = random.randint(1, 50)
 
-    if user_choice == "easy":
-        difficulty_setting = easy
-    elif user_choice == "medium":
-        difficulty_setting = medium
-    elif user_choice == "hard":
-        difficulty_setting = hard
-    else:
-        difficulty_setting = easy
+        if user_choice == "easy":
+            difficulty_setting = easy
+        elif user_choice == "medium":
+            difficulty_setting = medium
+        elif user_choice == "hard":
+            difficulty_setting = hard
+        else:
+            difficulty_setting = easy
 
-    return difficulty_setting
+        return difficulty_setting
 
-user_difficulty_choice = input("Pick a difficulty, easy, medium, hard: ")
+    user_difficulty_choice = input("Pick a difficulty, easy, medium, hard: ")
 
-computer_num = difficulty(user_difficulty_choice.lower())
+    computer_num = difficulty(user_difficulty_choice.lower())
 
-#User inputs guess, computer outputs response based on how close answer is to number, if guess is not a number within range computer lets the user know
+    #User inputs guess, computer outputs response based on how close answer is to number, if guess is not a number within range computer lets the user know
 
-user_guess = input("Guess a number! easy (1 - 5), medium (1 - 10), hard (1 - 50): ")
+    user_guess = input("Guess a number! easy (1 - 5), medium (1 - 10), hard (1 - 50): ")
 
-if int(user_guess) == int(computer_num):
-    print("Correct!")
-elif int(user_guess) > int(computer_num):
-    print("You guessed high.")
-    print("Computer had picked, ",int(computer_num), "try again.")
-elif int(user_guess) < int(computer_num):
-    print("You guessed low.")
-    print("Computer had picked: ", int(computer_num), "try again.")
-#User gets score reduction with each guess
-
-
-#after guess is correct final score is listed
+    if int(user_guess) == int(computer_num):
+        print("Correct!")
+    elif int(user_guess) > int(computer_num):
+        print("You guessed high.")
+        print("Computer had picked, ",int(computer_num), "try again.")
+    elif int(user_guess) < int(computer_num):
+        print("You guessed low.")
+        print("Computer had picked: ", int(computer_num), "try again.")
+    score += 1
+    #User gets score reduction with each guess
 
 
+    #after guess is correct final score is listed
 
 
-#later versions could have high score list
 
 
-#testing pull and push to github
+    #later versions could have high score list
+
+
+    #testing pull and push to github
 
 
 
